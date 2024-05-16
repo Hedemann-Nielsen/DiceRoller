3a1df36c476a40e235f9ace26c9833d2dd1ce7ed
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-05-16 08:28:11 +0200

    Flere spillere tilføjet

--- DiceRoller.js
+++ DiceRoller.js
@@ -1,91 +1,113 @@
-//hent alle html elementer
-let playerOneScore = document.getElementById("playerOneScore");
-let playerTwoScore = document.getElementById("playerTwoScore");
+// hent alle html elementer
 let roundNumber = document.getElementById("roundNumber");
 let messageContainer = document.getElementById("messageContainer");
-let trhowDicesBtn = document.getElementById("trhowDices");
+let throwDicesBtn = document.getElementById("throwDices");
 let resetGameBtn = document.getElementById("resetGame");
+let addPlayerBtn = document.getElementById("addPlayer");
+let resetPlayersBtn = document.getElementById("resetPlayers");
+let playerNameInput = document.getElementById("playerName");
+let playersContainer = document.getElementById("playersContainer");
 let dice1 = document.getElementById("dice1");
 let dice2 = document.getElementById("dice2");
 
-//variabler til at starte spillet
-let playerOneTotaltScore = 0;
-let playerTwoTotalScore = 0;
+// variabler til at starte spillet
+let players = [];
 let currentRound = 1;
-let isPlayerOne = true;
+let currentPlayerIndex = 0;
 
-//sætter rundenummer tekseten ind i elementet fra starten
+// sætter rundenummer tekseten ind i elementet fra starten
 roundNumber.innerText = ` ${currentRound} ud af 3`;
 
-//variabler der kan opdateres
+// variabler der kan opdateres
 let randomNumber1 = 0;
 let randomNumber2 = 0;
 let diceSum = 0;
 
-//spillernes verdier
+// spillernes værdier
 class Player {
 	constructor(playerName) {
 		this.playerName = playerName;
 		this.totalScore = 0;
-		this.isPlayerTurn = false;
+		this.element = document.createElement("div");
+		this.element.className = "flexContainer";
+		this.scoreElement = document.createElement("p");
+		this.updateScoreElement();
+		this.element.innerHTML = `<p>${this.playerName} har:</p>`;
+		this.element.appendChild(this.scoreElement);
+		playersContainer.appendChild(this.element);
 	}
 
 	updateScore(roundScore) {
 		this.totalScore += roundScore;
+		this.updateScoreElement();
+	}
+
+	updateScoreElement() {
+		this.scoreElement.innerText = `${this.totalScore} point`;
+	}
+
+	remove() {
+		playersContainer.removeChild(this.element);
 	}
 }
-// opret spillerne
-let playerOne = new Player("PlayerOne");
-let playerTwo = new Player("PlayerTwo");
 
-//Knap events
-trhowDicesBtn.addEventListener("click", rollDices);
+// Knap events
+throwDicesBtn.addEventListener("click", rollDices);
 resetGameBtn.addEventListener("click", resetGame);
+addPlayerBtn.addEventListener("click", addPlayer);
+resetPlayersBtn.addEventListener("click", resetPlayers);
+playerNameInput.addEventListener("keypress", function (event) {
+	if (event.key === "Enter") {
+		addPlayer();
+	}
+});
 
-//Kast terningerne
+// Tilføj spiller
+function addPlayer() {
+	const playerName = playerNameInput.value.trim();
+	if (playerName) {
+		players.push(new Player(playerName));
+		playerNameInput.value = "";
+	}
+}
+
+// Nulstil spillere
+function resetPlayers() {
+	players.forEach((player) => player.remove());
+	players = [];
+	resetGame();
+}
+
+// Kast terningerne
 function rollDices() {
-	randomNumber1 = Math.ceil(Math.random(1) * 6);
-	randomNumber2 = Math.ceil(Math.random(1) * 6);
+	if (players.length < 2) {
+		messageContainer.innerText =
+			"Tilføj mindst 2 spillere for at starte spillet";
+		return;
+	}
+
+	randomNumber1 = Math.ceil(Math.random() * 6);
+	randomNumber2 = Math.ceil(Math.random() * 6);
 	dice1.src = `./assets/dice${randomNumber1}.png`;
 	dice2.src = `./assets/dice${randomNumber2}.png`;
 	console.log("terning kastet");
 
 	diceSum = randomNumber1 + randomNumber2;
 
-	if (isPlayerOne) {
-		playerOne.updateScore(diceSum);
-		updatePlayerScore("PlayerOne", diceSum);
-	} else {
-		playerTwo.updateScore(diceSum);
-		updatePlayerScore("PlayerTwo", diceSum);
+	let currentPlayer = players[currentPlayerIndex];
+	currentPlayer.updateScore(diceSum);
+	messageContainer.innerText = `${currentPlayer.playerName} modtog ${diceSum} point`;
+
+	currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+
+	if (currentPlayerIndex === 0) {
 		updateRoundNumber();
 	}
+
+	throwDicesBtn.innerText = `${players[currentPlayerIndex].playerName} - kaster terningen`;
 }
 
-//opdater spillernes point
-function updatePlayerScore(player, diceSum) {
-	switch (player) {
-		case "PlayerOne":
-			playerOneScore.innerText = `${playerOne.totalScore} point`;
-			messageContainer.innerText = `Spiller 1 modtog ${diceSum} point`;
-			isPlayerOne = false;
-			trhowDicesBtn.innerText = "Spiller 2 - kaster terningen";
-			break;
-
-		case "PlayerTwo":
-			playerTwoScore.innerText = `${playerTwo.totalScore} point`;
-			messageContainer.innerText = `Spiller 2 modtog ${diceSum} point`;
-			isPlayerOne = true;
-			trhowDicesBtn.innerText = "Spiller 1 - kaster terningen";
-
-			break;
-
-		default:
-			console.error("switch - no value matches");
-	}
-}
-
-//updater runden
+// updater runden
 function updateRoundNumber() {
 	currentRound++;
 	if (currentRound <= 3) {
@@ -95,7 +117,7 @@ function updateRoundNumber() {
 	}
 }
 
-//Definer konfetti
+// Definer konfetti
 function showConfetti() {
 	confetti({
 		particleCount: 500,
@@ -104,56 +126,39 @@ function showConfetti() {
 	});
 }
 
-//start spillet forfra
+// start spillet forfra
 function endGame() {
-	if (playerOne.totalScore > playerTwo.totalScore) {
-		messageContainer.innerText = "Spiller 1 vinder spillet!";
-		messageContainer.style.fontSize = "2em"; // Gør teksten større
-		messageContainer.style.fontWeight = "bold"; // Gør teksten fed
-		showConfetti();
-	} else if (playerTwo.totalScore > playerOne.totalScore) {
-		messageContainer.innerText = "Spiller 2 vinder spillet!";
-		messageContainer.style.fontSize = "2em"; // Gør teksten større
-		messageContainer.style.fontWeight = "bold"; // Gør teksten fed
-		showConfetti();
-	} else {
-		messageContainer.innerText = "Spillet endte uafgjort!";
-		messageContainer.style.fontSize = "2em"; // Gør teksten større
-		messageContainer.style.fontWeight = "bold"; // Gør teksten fed
-	}
+	let winner = players.reduce((prev, current) =>
+		prev.totalScore > current.totalScore ? prev : current
+	);
+	messageContainer.innerText = `${winner.playerName} vinder spillet!`;
+	messageContainer.style.fontSize = "2em"; // Gør teksten større
+	messageContainer.style.fontWeight = "bold"; // Gør teksten fed
+	showConfetti();
 
-	playerOneTotaltScore = 0;
-	playerTwoTotalScore = 0;
-	playerOneScore.innerText = `0 point`;
-	playerTwoScore.innerText = `0 point`;
-	dice1.src = `./assets/dice1.png`;
-	dice2.src = `./assets/dice1.png`;
-
-	currentRound = 1;
-	isPlayerOne = true;
-	roundNumber.innerText = ` ${currentRound} ud af 3`;
-
-	trhowDicesBtn.disabled = true;
-	trhowDicesBtn.style.backgroundColor = "#d3d3d3"; // Lys grå farve
-	trhowDicesBtn.style.color = "#808080"; // Mørk grå farve for tekst
+	throwDicesBtn.disabled = true;
+	throwDicesBtn.style.backgroundColor = "#d3d3d3"; // Lys grå farve
+	throwDicesBtn.style.color = "#808080"; // Mørk grå farve for tekst
 }
 
-//reset spillet
+// reset spillet
 function resetGame() {
-	playerOne.totalScore = 0;
-	playerTwo.totalScore = 0;
-	playerOneScore.innerText = `0 point`;
-	playerTwoScore.innerText = `0 point`;
+	players.forEach((player) => (player.totalScore = 0));
+	players.forEach((player) => player.updateScoreElement());
 	messageContainer.innerText = `Tryk på "Kast terningerne" for at starte`;
 	dice1.src = `./assets/dice1.png`;
 	dice2.src = `./assets/dice1.png`;
 
 	currentRound = 1;
-	isPlayerOne = true;
+	currentPlayerIndex = 0;
 	roundNumber.innerText = ` ${currentRound} ud af 3`;
-	trhowDicesBtn.disabled = false;
+	throwDicesBtn.disabled = false;
 	messageContainer.style.fontSize = "20px"; // Gør teksten større
 	messageContainer.style.fontWeight = "normal"; // Gør teksten fed
-	trhowDicesBtn.style.backgroundColor = "#006ae8"; // Lys grå farve
-	trhowDicesBtn.style.color = "aliceblue"; // Mørk grå farve for tekst
+	throwDicesBtn.style.backgroundColor = "#006ae8"; // Lys grå farve
+	throwDicesBtn.style.color = "aliceblue"; // Mørk grå farve for tekst
+
+	if (players.length > 0) {
+		throwDicesBtn.innerText = `${players[currentPlayerIndex].playerName} - kaster terningen`;
+	}
 }
